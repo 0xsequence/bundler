90317ca804ebc0e34c167395c78b6b44874dbb22
William Hua <william@attente.ca>
2024-02-19 16:14:06 +0100

    Fix imports in OperationValidator

--- contracts/gen/operationvalidator/operationvalidator.go
+++ contracts/gen/operationvalidator/operationvalidator.go
@@ -8,12 +8,12 @@ import (
 	"math/big"
 	"strings"
 
-	ethereum "github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/event"
+	ethereum "github.com/0xsequence/ethkit/go-ethereum"
+	"github.com/0xsequence/ethkit/go-ethereum/accounts/abi"
+	"github.com/0xsequence/ethkit/go-ethereum/accounts/abi/bind"
+	"github.com/0xsequence/ethkit/go-ethereum/common"
+	"github.com/0xsequence/ethkit/go-ethereum/core/types"
+	"github.com/0xsequence/ethkit/go-ethereum/event"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
