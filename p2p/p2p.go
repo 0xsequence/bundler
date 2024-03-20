4eee0356c254be7eed315f3b7d4659ce0be5bc78
Agusx1211 <agusgit@pm.me>
2024-03-20 14:43:47 +0000

    Simpler pubsub topic

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,9 +3,9 @@ package p2p
 import "math/big"
 
 const (
-	DiscoveryNamespace = "erc5189-bundler"
+	DiscoveryNamespace = "erc5189-mempool"
 
-	PubsubTopicPrefix = "erc5189-bundler-op-mempool-"
+	PubsubTopicPrefix = "erc5189-mempool"
 )
 
 func PubsubTopic(chainID *big.Int) string {
